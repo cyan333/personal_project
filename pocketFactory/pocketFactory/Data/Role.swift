8c9650fd37b715ae4ce1ad94736a318b2f7442ff
cyan333 <iamcyanxie@gmail.com>
2018-04-29 11:18:09 -0400

    one type of cell works

---
+++ pocketFactory/pocketFactory/Data/Role.swift
@@ -0,0 +1,43 @@
+//
+//  Role.swift
+//  pocketFactory
+//
+//  Created by NingFangming on 4/27/18.
+//  Copyright © 2018 fangming. All rights reserved.
+//
+
+import Foundation
+class Role {
+    var name: String
+    var positionLevel: Int
+    var ownerName: String
+    var updateBy: String
+    var canCreateTask: Bool
+    var canCreateProduct: Bool
+    
+    
+    init(name: String,
+         positionLevel: Int,
+         ownerName: String,
+         updateBy: String,
+         canCreateTask: Bool,
+         canCreateProduct: Bool) {
+        self.name = name
+        self.positionLevel = positionLevel
+        self.ownerName = ownerName
+        self.updateBy = updateBy
+        self.canCreateTask = canCreateTask
+        self.canCreateProduct = canCreateProduct
+
+    }
+    
+    init() {
+        self.name = ""
+        self.positionLevel = 1
+        self.ownerName = ""
+        self.updateBy = ""
+        self.canCreateTask = false
+        self.canCreateProduct = false
+    }
+    
+}
--- pocketFactory/pocketFactory/ViewController/ManageMainVC.swift
+++ pocketFactory/pocketFactory/ViewController/ManageMainVC.swift
@@ -9,10 +9,43 @@
 import Foundation
 import UIKit
 
-class ManageMainVC: UIViewController {
+class ManagePeopleProfileNameDepartmentCell: UITableViewCell {
+    
+    @IBOutlet var peopleProfileImage: UIImageView!
+    @IBOutlet var peopleNameLabel: UILabel!
+    @IBOutlet var peopleDepartmentLabel: UILabel!
+    @IBOutlet var peopleNextArrowImage: UIImageView!
+    @IBOutlet var peoplePositionLabel: UILabel!
+    
+    
+}
+
+class ManageMainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let list = ["Milk", "Monkey", "Break"]
+    let product = ["40W Charger", "Battery Charger", "Waterproof Charger"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ManagePeopleProfileNameDepartmentCell") as! ManagePeopleProfileNameDepartmentCell
+        cell.peopleNameLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70.0
+    }
     
     override func viewDidLoad() {
+        
+        
         setupNavBar()
+        
+        
     }
     
     func setupNavBar() {
@@ -21,6 +54,9 @@ class ManageMainVC: UIViewController {
         navigationItem.hidesSearchBarWhenScrolling = false
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
     @IBAction func switchManageMainTbViewAction(_ sender: UISegmentedControl) {
     }
